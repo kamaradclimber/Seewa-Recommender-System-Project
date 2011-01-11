491ffd40c1a152e0dd5e2fe609fdf3f043672f24
FlorentVermeulen <florent.vermeulen@student.ecp.fr>
2011-01-11 11:19:18 +0100

    initialisation des classes pour le modèle bayesion (graphe d'utilisateurs)

---
+++ prototype1/AlgoLegerBayes.java
@@ -0,0 +1,14 @@
+
+public class AlgoLegerBayes extends AlgoLeger {
+
+	public AlgoLegerBayes() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Recommendation answers(Request req) throws ExceptionRecoNotValid {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ prototype1/BayesRequest.java
@@ -0,0 +1,9 @@
+
+public class BayesRequest extends Request {
+
+	public BayesRequest(String req) {
+		super(req);
+		// TODO Auto-generated constructor stub
+	}
+
+}
