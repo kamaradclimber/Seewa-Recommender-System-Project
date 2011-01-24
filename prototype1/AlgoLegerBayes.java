a97f0b29d85ad3a183829b7ccea25cbb9824396a
Grégoire Seux <kamaradclimber@gmail.com>
2011-01-24 09:23:00 +0100

    fix a small bug in comaprefunction due to initialisation

--- prototype1/AlgoLegerBayes.java
+++ prototype1/AlgoLegerBayes.java
@@ -32,10 +32,10 @@ public final class AlgoLegerBayes extends AlgoLeger {
 		DataUserNode user;
 		if (req.getUrl()=="test" ) 
 		{
-			//géné des Upages
+			//gï¿½nï¿½ des Upages
 			//DataUPage jeanMichLeMonde= new DataUPage(new ObjectId(), 0.7, "www.lemonde.fr");
 			DataUPage jeanMichLeFigaro= new DataUPage(new ObjectId(), 0.8, "www.lefigaro.fr");
-			DataUPage jeanMichLEquipe= new DataUPage(new ObjectId(), 0.5, "www.léquipe.fr");
+			DataUPage jeanMichLEquipe= new DataUPage(new ObjectId(), 0.5, "www.lï¿½quipe.fr");
 			DataUPage jeanMichLinux= new DataUPage(new ObjectId(), 0.1, "www.linux.org");
 			
 			DataUPage leGeekLinux= new DataUPage(new ObjectId(), 0.8, "www.linux.org");
@@ -45,8 +45,8 @@ public final class AlgoLegerBayes extends AlgoLeger {
 			
 			DataUPage jeanJauresLeMonde= new DataUPage(new ObjectId(), 0.5, "www.lemonde.fr");
 			DataUPage jeanJauresLeFigaro= new DataUPage(new ObjectId(), 0.3, "www.lefigaro.fr");
-			DataUPage jeanJauresLEquipe= new DataUPage(new ObjectId(), 0.6, "www.léquipe.fr");
-			DataUPage jeanJauresLHuma= new DataUPage(new ObjectId(), 0.9, "www.lhumanité.fr");
+			DataUPage jeanJauresLEquipe= new DataUPage(new ObjectId(), 0.6, "www.lï¿½quipe.fr");
+			DataUPage jeanJauresLHuma= new DataUPage(new ObjectId(), 0.9, "www.lhumanitï¿½.fr");
 			
 			ArrayList<DataUPage> jeanMichUPage= new ArrayList<DataUPage>();
 			jeanMichUPage.add(jeanMichLinux);
@@ -66,11 +66,11 @@ public final class AlgoLegerBayes extends AlgoLeger {
 			jeanJauresUPage.add(jeanJauresLEquipe);
 			jeanJauresUPage.add(jeanJauresLHuma);
 			
-			//on crée les persos
+			//on crï¿½e les persos
 			DataUserNode jeanMich = new DataUserNode("jeanMich", new ObjectId(), new ArrayList<DataUserRelation>()  , jeanMichUPage);
 			DataUserNode leGeek = new DataUserNode("leGeek", new ObjectId(), new ArrayList<DataUserRelation>()  , leGeekUPage);
 			DataUserNode jeanJaures = new DataUserNode("JeanJaures", new ObjectId(), new ArrayList<DataUserRelation>()  , jeanJauresUPage);
-			//on implémente les liens d'amitié
+			//on implï¿½mente les liens d'amitiï¿½
 			DataUserRelation jmfriends = new DataUserRelation(leGeek);
 			DataUserRelation lgfriends = new DataUserRelation(jeanMich);
 			
@@ -116,14 +116,14 @@ public final class AlgoLegerBayes extends AlgoLeger {
 		TreeSet<Composite> bestReco = new TreeSet<Composite>(); //on stocke les trois meilleurs proba  
 		int nbReco=Math.min(10, pages.size());
 		
-		for (int j=0; j<nbReco; j++) bestReco.add(new Composite(null,null,-1));
+		for (int j=0; j<nbReco; j++) bestReco.add(new Composite(null,null,Integer.MIN_VALUE));
 		
 		//on va ensuite calculer toutes les probabilitÃ©s 
 		for (ArrayList<AlgoLegerBayes.Composite> cc : pages.values()) { //il y a peut etre une optimisation a faire sur la facon dont on stocke et parcourt cette table de hashage
 			for(Composite c :cc) {
 				c.crossProbability =  c.crossProbability / c.user.uPageMean * c.page.pageRank;
 				System.out.println( c.page.getUrl()+" : " + c.crossProbability);
-				bestReco.add(c);//TODO : IMPORTANT si deux pages ont la même proba, on les écrase!!!
+				bestReco.add(c);//TODO : IMPORTANT si deux pages ont la mï¿½me proba, on les ï¿½crase!!!
 				bestReco.remove(bestReco.first());
 			}
 		}
@@ -131,14 +131,14 @@ public final class AlgoLegerBayes extends AlgoLeger {
 //		TreeMap<Double,String> bestsReco = new TreeMap<Double,String>(); //on stocke les trois meilleurs proba  
 //		for(int i=0;i<10;i++){bestsReco.put((double)0, "");}
 //		//on initialise Ã  3 meilleures reco, nombre qu'on maintient ensuite
-//		//TODO : en faite non, on écrase toujours la même clé.
+//		//TODO : en faite non, on ï¿½crase toujours la mï¿½me clï¿½.
 //		
 //		//on va ensuite calculer toutes les probabilitÃ©s 
 //		for (ArrayList<AlgoLegerBayes.Composite> cc : pages.values()) { //il y a peut etre une optimisation a faire sur la facon dont on stocke et parcourt cette table de hashage
 //			for(Composite c :cc) {
 //				double proba =  c.crossProbability / c.user.uPageMean * c.page.pageRank;
 //				System.out.println( c.page.getUrl()+" : " + proba);
-//				bestsReco.put(proba, c.page.getUrl());//TODO : IMPORTANT si deux pages ont la même proba, on les écrase!!!
+//				bestsReco.put(proba, c.page.getUrl());//TODO : IMPORTANT si deux pages ont la mï¿½me proba, on les ï¿½crase!!!
 //				bestsReco.remove(bestsReco.firstKey()); //on maintient seulement 3 meilleures
 //			}
 //		}
@@ -188,6 +188,10 @@ public final class AlgoLegerBayes extends AlgoLeger {
 			if (this.crossProbability < arg0.crossProbability) return -1;
 			if (this.crossProbability > arg0.crossProbability) return 1;
 			//same proba;
+			System.out.println(this.page);
+			System.out.println(arg0.page);
+			if (this.page==null && arg0.page==null) return 0;
+			assert (this.page !=null && arg0.page!=null);
 			return this.page.getUrl().compareTo(arg0.page.getUrl());
 		}
 
