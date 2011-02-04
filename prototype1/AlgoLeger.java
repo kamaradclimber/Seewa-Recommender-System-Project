57a3d242f3ac74a788b00c6b594966e67ccd1b4b
Florent Vermeulen <florent.vermeulen@student.ecp.fr>
2011-02-04 14:21:40 +0100

    modif dispatcher, aggregateur pour améliorer l'évolutivité

--- prototype1/AlgoLeger.java
+++ prototype1/AlgoLeger.java
@@ -4,5 +4,10 @@ public abstract class AlgoLeger {
 	public abstract Recommendation answers(Request req) throws ExceptionRecoNotValid;
 		//exemple pour un algo de recommendation
 		//return new Recommendation();
+
+	public Request transform(Request req) {
+		// crée une requete adaptée à l'algo
+		return req;
+	}
 	
 }
--- prototype1/Recommendeur.java
+++ prototype1/Recommendeur.java
@@ -2,6 +2,35 @@ import java.util.List;
 
 
 public class Recommendeur {
+	
+	static private DispatcherAlgoLeger dispatch;
+	static private Aggregateur aggreg;
+	static private Verificateur verificateur;
+	
+	static{
+		dispatch = new DispatcherAlgoLeger();
+		aggreg = new Aggregateur();
+		verificateur = new Verificateur();
+	}
+	
+	public Recommendeur()
+	{
+		super();
+	}
+	
+	public void addAlgo(String algo)
+	{
+		try{
+			Class<AlgoLeger> c = (Class<AlgoLeger>) Class.forName(algo);
+			AlgoLeger algoInstance= c.newInstance();
+
+			dispatch.addAlgo(algoInstance);
+			aggreg.addAlgo(algoInstance);
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
  
 	public Recommendation recommendMeSomeThing(Request req) throws ExceptionRecoNotValid {
 		DispatcherAlgoLeger dispatch = new DispatcherAlgoLeger();
@@ -9,7 +38,7 @@ public class Recommendeur {
 		Verificateur verificateur    = new Verificateur();
 		
 		List<Recommendation> recos;
-		recos = dispatch.dispatches(req);
+		recos = dispatch.dispatch(req);
 		Recommendation reco  =  aggreg.merges(recos);
 		verificateur.verifies(reco);
 		return reco;
