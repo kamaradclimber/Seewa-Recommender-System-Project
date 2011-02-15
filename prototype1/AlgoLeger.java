0da35b86d3272cac9b9682dd7d36d3e36ee9351d
Grégoire Seux <kamaradclimber@gmail.com>
2011-02-15 09:15:56 +0100

    proprification du code

--- prototype1/AlgoLeger.java
+++ prototype1/AlgoLeger.java
@@ -5,9 +5,11 @@ public abstract class AlgoLeger {
 		//exemple pour un algo de recommendation
 		//return new Recommendation();
 
-	public Request transform(Request req) {
-		// crée une requete adaptée à l'algo
-		return req;
-	}
+	//TODO : décommenter cette méthode et rajouter un appel dans le dispatcher
+//	public Request transform(Request req) {
+//	//cette méthode permet de tranformer la requete générale en reuqet pour lalgo si besoin
+//		// crée une requete adaptée à l'algo
+//		return req;
+//	}
 	
 }
