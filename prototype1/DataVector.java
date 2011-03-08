e3656b973851dc8710cbabe8ed51cadbb6c38883
Florent Vermeulen <florent.vermeulen@student.ecp.fr>
2011-03-08 11:19:11 +0100

    calcul utr

---
+++ prototype1/DataVector.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import org.bson.types.ObjectId;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+
+
+
+@SuppressWarnings("serial")
+public class DataVector extends Hashtable<String, Float> implements Data  {
+	String name;
+	private int arrayId = 0;
+	private ObjectId userID; //on stocke l'id de l'user qui est associé au vector, si on parle dun user sinon il est null
+	//DONE: et si on mettait userID, ou juste "Objet user;"? c'est fait (greg)
+	
+	private static int IdCount =0;
+	
+	
+	public DataVector(ObjectId userID ) {
+		super();
+		this.arrayId = IdCount;
+		IdCount++;
+		this.userID = userID; // ceci correspond a l'id de l'utilisateur auquel le data est eventuellement lié
+		BasicDBObject query = new BasicDBObject("user",userID);
+		
+		DBCursor pageviewedbyuser = Interprete.upages.find(query);
+		
+		Hashtable<String, Integer> nbPagePerTheme = new Hashtable<String, Integer>();
+
+		BasicDBList themes=null;
+		Float pageRank = new Float(0);
+		while (pageviewedbyuser.hasNext())
+		{
+			DBObject upage = pageviewedbyuser.next();
+			try {
+				  themes = (BasicDBList) upage.get("themes");
+				  pageRank = (Float) upage.get("pageRank");
+				  name = (String ) upage.get("userName");
+			} catch (Exception e){
+				System.out.println("failed :"+upage.get("themes"));
+			}
+			for (Object listItem:themes)
+			{
+				String themeName = ((BasicDBObject) listItem).getString("name");
+				if (!this.containsKey(themeName)){
+					this.put(themeName, pageRank);
+					nbPagePerTheme.put(themeName, 1);
+				}else{
+					this.put(themeName, this.get(themeName)+pageRank);
+					nbPagePerTheme.put(themeName, nbPagePerTheme.get(themeName)+1);
+				}
+			}
+		}
+	
+		for (String theme : nbPagePerTheme.keySet())
+		{
+			this.put(theme, this.get(theme)/nbPagePerTheme.get(theme));
+		}
+		
+		
+	}
+	
+	
+	
+	public int getArrayId() {
+		return this.arrayId;
+	}
+	
+	public void setArrayId(int id) {
+		this.arrayId = id;
+	}
+	
+	public boolean equals(Object o) {
+		try {
+			DataVector v = (DataVector) o;
+			for(String key : this.keySet()) {
+				if (!v.containsKey(key) || v.get(key) != this.get(key))  {
+					return false;
+				}
+			}
+			return (v.size()==this.size());
+		} catch (ClassCastException ex) {
+			return false;
+		}
+	}
+
+
+
+	
+	public float getOrZero(String key) { //renvoie la valeur si la clé existe et zéro sinon
+		if (!this.containsKey(key)) {
+			return 0;
+		}
+		return this.get(key);
+	}
+
+
+	@Override
+	public ObjectId getMongoId() {
+		return this.userID;
+	}
+
+
+
+	@Override
+	public String toString() {
+		String tostring = "DataVector [arrayId=" + arrayId + ", name=" + name
+				+ ", userID=" + userID + "]\n";
+		
+		for (String theme : this.keySet())
+		{
+			tostring += theme + " : " + this.get(theme) +"\n";
+		}
+		
+		return tostring;
+	}
+	
+}
