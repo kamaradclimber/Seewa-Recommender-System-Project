172058d70b9d556ac688d18b1214163c27fc7599
Grégoire Seux <kamaradclimber@gmail.com>
2011-03-26 12:46:10 +0100

    ajout dun fichier de test de vitesse

---
+++ speed_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for ((i = 0; i < 100; i += 1))
+do
+    echo $i;
+    wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000" & wget -q -O reco "http://138.195.76.136/recommend?userid=ab033f4d90ad4d1400000000"
+done 
